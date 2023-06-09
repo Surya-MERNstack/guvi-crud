5f333f7d2274e8be194451b274703c639a811ad3
Surya-MERNstack <charusurya17@gmail.com>
2023-06-09 18:02:07 +0530

    some add

--- index.js
+++ index.js
@@ -1,28 +1,26 @@
-const express = require('express');
-const mongose = require('mongoose');
+const express = require("express");
+const mongose = require("mongoose");
 
-const dburl = 'mongodb://0.0.0.0:27017/DemoStudent';
+const dburl = "mongodb://0.0.0.0:27017/DemoStudent";
 
-mongose.connect(dburl , {useNewUrlParser  : true});
+mongose.connect(dburl, { useNewUrlParser: true });
 
 const con = mongose.connection;
 
-const app   = express();
-app.use(express.json())
-app.use(express.urlencoded({extended :  false}))
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 try {
-    con.on('open' , () => {
-        console.log('mongose is connected');
-    })
-}catch(err){
-  console.log(ReferenceError , err);
+  con.on("open", () => {
+    console.log("mongose is connected");
+  });
+} catch (err) {
+  console.log(ReferenceError, err);
 }
 
+const studentRouter = require("./router/students");
+app.use("/students", studentRouter);
 
-const studentRouter = require('./router/students');
-app.use("/students",studentRouter)
-
-app.listen(3020 , () => {
-    console.log('Server is running port 3000!!')
-})
-
+app.listen(3020, () => {
+  console.log("Server is running port 3020!!");
+});
